b5aa028e260a9d953003557e1db865e1d9d72c42
Harinivigneshwaran <160514199+Harinivigneshwaran@users.noreply.github.com>
2024-07-28 18:35:25 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -29,16 +29,16 @@ def calculator():
             num2 = float(input("Enter second number: "))
 
             if choice == '1':
-                print("{num1} + {num2} = {add(num1, num2)}")
+                print(f"{num1} + {num2} = {add(num1, num2)}")
 
             elif choice == '2':
-                print("{num1} - {num2} = {subtract(num1, num2)}")
+                print(f"{num1} - {num2} = {subtract(num1, num2)}")
 
             elif choice == '3':
-                print("{num1} * {num2} = {multiply(num1, num2)}")
+                print(f"{num1} * {num2} = {multiply(num1, num2)}")
 
             elif choice == '4':
-                print("{num1} / {num2} = {divide(num1, num2)}")
+                print(f"{num1} / {num2} = {divide(num1, num2)}")
 
         else:
             print("Invalid input")
